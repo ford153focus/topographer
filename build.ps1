bc054aa67a67bb43abbf14117de0c4939e06a3e6
Joe Ford <ford153focus@gmail.com>
2019-05-09 08:42:46 +0300

    update publish command

--- build.ps1
+++
@@ -1,3 +0,0 @@
-dotnet clean
-dotnet restore
-dotnet build --output "$((Get-Item -Path ".\").FullName)\bin\Release\win10-x64" --configuration Release --runtime win10-x64 --verbosity diag
---
+++ publish.sh
@@ -0,0 +1,2 @@
+time dotnet publish --output bin/Release/linux-x64 --runtime linux-x64 --self-contained --verbosity minimal
+time dotnet publish --output bin/Release/win10-x64 --runtime win10-x64 --self-contained --verbosity minimal

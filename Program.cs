5e30aff3060c2e5af27688cbf306883734392f45
Joe Ford <ford153focus@gmail.com>
2019-05-09 08:43:05 +0300

    Extend comments

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace topographer
     {
         static void Main(string[] args)
         {
+            /// Get site address
             Console.WriteLine("Enter host. \"http://example.com/\" for example.");
             String indexUrl = Console.ReadLine();
             // String indexUrl = "https://fittorg.ru/";
@@ -16,30 +17,37 @@ namespace topographer
 
             var web = new HtmlWeb();
 
+            /// Here we will save links that must be parsed
             var linksOrder = new List<String>();
+            linksOrder.Add(indexUrl);
+            /// Here we will save links that already parsed
             var parsedLinks = new List<String>();
 
+            /// Prepare xml document for sitemap
             XmlDocument sitemap = new XmlDocument();
             XmlElement urlset = (XmlElement)sitemap.AppendChild(sitemap.CreateElement("urlset"));
             urlset.SetAttribute("xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9");
 
-            linksOrder.Add(indexUrl);
-
+            /// Sitemap can't contain over 50000 records
             while (linksOrder.Count > 0 && parsedLinks.Count < 50000)
             {
                 Console.WriteLine("Parsing {0}", linksOrder[0]);
 
+                /// Load url from order
                 var doc = web.Load(linksOrder[0]);
+
+                /// Grab all links from loaded page
                 HtmlNodeCollection freshLinks = doc.DocumentNode.SelectNodes("//a");
 
+                /// Put all grabbed links in order
                 foreach (var freshLink in freshLinks)
                 {
                     var freshLinkHref = freshLink.GetAttributeValue("href", "");
                     var freshLinkUri = new Uri(indexUrlUri, freshLinkHref);
-                    // Check for host
+                    /// Check for host
                     if (indexUrlUri.Host == freshLinkUri.Host)
                     {
-                        // Check existence in list
+                        /// Check existence in list
                         if (linksOrder.FindIndex(link => link == freshLinkUri.AbsoluteUri) == -1 && 
                             parsedLinks.FindIndex(link => link == freshLinkUri.AbsoluteUri) == -1)
                         {
@@ -48,13 +56,11 @@ namespace topographer
                         }
                     }
                 }
-                // Add to parsed and remove from order
-                parsedLinks.Add(linksOrder[0]);
-
+                /// Add link to list of parsed and remove from order
                 XmlElement url = (XmlElement)urlset.AppendChild(sitemap.CreateElement("url"));
                 XmlElement loc = (XmlElement)url.AppendChild(sitemap.CreateElement("loc"));
                 loc.InnerText = linksOrder[0];
-
+                parsedLinks.Add(linksOrder[0]);
                 linksOrder.RemoveAt(0);
             }
 

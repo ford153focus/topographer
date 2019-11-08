9ababbc3c0909fc038d04580287c263c27cb22ff
Joe Ford <ford153focus@gmail.com>
2019-11-08 16:41:54 +0300

    crossplatform: use path separator char

--- Config.cs
+++ Config.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Text;
 
 namespace topographer
@@ -6,7 +7,7 @@ namespace topographer
     public static class Config
     {
         public static string targetHost = "http://example.com/";
-        public static string savePath = Environment.CurrentDirectory + "\\sitemap.xml";
+        public static string savePath = Environment.CurrentDirectory + Path.DirectorySeparatorChar + "sitemap.xml";
         public static bool verbose = false;
     }
 }
